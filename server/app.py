7efe38252e517e0fc3d2e6d5ab94c8c3f7fd64bc
Lexy Perry <lexy.perry61022@gmail.com>
2025-07-30 22:06:07 -0400

    Implement Flask API with session-based authentication and article access control

--- server/app.py
+++ server/app.py
@@ -20,6 +20,7 @@ api = Api(app)
 
 class ClearSession(Resource):
 
+
     def delete(self):
     
         session['page_views'] = None
@@ -47,10 +48,41 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+
+        user = User.query.filter_by(username=username).first()
+        
+        if user:
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 200
+        else: 
+            return {'error': 'User not found'}, 404
+        
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            return UserSchema().dump(user), 200 
+        else: 
+            return {}, 401
+
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
